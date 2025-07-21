d942a338461a3e82f91fc5939616dadb68c02496
Dulara Devinda <dularad@diamond.lk>
2025-07-21 09:01:35 +0530

    updated

---
+++ user/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserPanel from './components/SurveyUserPanel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserPanel/>
+    </div>
+  );
+}
+
+export default App;
